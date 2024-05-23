88ce338ffdf637a857dd46d03d9914cc0368dc38
Osminogr <osminogr@yandex.ru>
2024-05-24 06:41:00 +0700

    on pages test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //basePath: "/kop-site",
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-}
+  distDir: 'build',
+  assetPrefix: '/kop-site/',
+};
 
 module.exports = nextConfig
